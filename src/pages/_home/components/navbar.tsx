849e0e12cca2ade9f8587eb9946148ad1eb5776c
DiegoJohnsonL <diegojohnson46@gmail.com>
2024-08-05 11:09:08 -0300

    update: nav

--- src/pages/_home/components/navbar.tsx
+++ src/pages/_home/components/navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
           gsap.to(navbar, {
             backgroundColor: "white",
             color: "#51362D",
-
+            duration: 0.1,
             onStart: () => {
               setPadding("py-4 md:py-6");
             },
@@ -34,7 +34,7 @@ export default function Navbar() {
           gsap.to(navbar, {
             backgroundColor: "transparent",
             color: "white",
-
+            duration: 0.1,
             onStart: () => {
               setPadding("py-6 md:py-10");
             },
@@ -46,10 +46,10 @@ export default function Navbar() {
 
   return (
     <header ref={navRef} className={`relative z-50 px-4 md:px-28 ${padding} text-white transition-all duration-500`}>
-      <nav className="flex gap-4 md:gap-8 justify-center">
+      <nav className="flex gap-5 md:gap-8 justify-center">
         {navItems.map((item, index) => (
           <a href={item.href} key={index}>
-            <a className={`md:text-lg underline-offset-4 hover:underline`}>{item.label}</a>
+            <a className={`text-sm md:text-lg underline-offset-4 hover:underline`}>{item.label}</a>
           </a>
         ))}
       </nav>
